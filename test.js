8fcffb0ef27f97c7345aaaade75ef6783b512966
PCTai <pntai121101@gmail.com>
2021-10-23 14:35:48 +0700

    upload content video

---
+++ test.js
@@ -0,0 +1,18 @@
+// var homeList = document.getElementById("list-Home")
+
+// var barLeft=document.getElementById("bar-left")
+
+// homeList.addEventListener('click',function(){
+//     var barTop=document.getElementById("bar-top");
+//     var isClose= barLeft.style.display === 'block';
+//     var isClickBarTop= barTop.style.display === 'block';
+//     barLeft.style.display='none';
+//     if(isClose){
+//         barLeft.style.display='none';
+//         barTop.style.left='0';
+//     }
+//     else {
+//         barLeft.style.display='block';
+//         barTop.style.left='230px';
+//     }
+// })
